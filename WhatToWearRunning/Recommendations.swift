55f2b63bc8505cef82dc0ba0f2472b460c65dced
Jean Bahnik <jeanbahnik@gmail.com>
2016-01-19 19:17:35 -0600

    new recommendation structure

--- WhatToWearRunning/Recommendations.swift
+++ WhatToWearRunning/Recommendations.swift
@@ -9,27 +9,71 @@
 import Foundation
 import ForecastIO
 
+enum Gear {
+    enum Head {
+        case CapSunglasses, LightHat, HeavyHat, FaceMask
+        var description: String {
+            switch self {
+            case .CapSunglasses: return "Cap or Sunglasses"
+            case .LightHat: return "Light Hat"
+            case .HeavyHat: return "Heavy Hat"
+            case .FaceMask: return "Face Mask"
+            }
+        }
+    }
+}
+
 class Recommendations {
     
     var forecast: Forecast?
+    var recommendedOutfit = [String]()
     
     init(forecast: Forecast) {
         self.forecast = forecast
+        selectHeadGear()
     }
     
-    func summaryRecommendation() -> String {
-        var summaryRecommendation: String?
-        if let currently = forecast?.currently {
-            if let summary = currently.summary {
-                switch summary {
-                case "Mostly Cloudy":
-                    summaryRecommendation = "It's mostly cloudy so it may rain"
-                default:
-                    summaryRecommendation = "No instructions"
+//    func outfitRecommendation() -> String {
+//        var outfitRecommendation: String?
+//        if let currently = forecast?.currently {
+//            if let summary = currently.summary {
+//                switch summary {
+//                case "Mostly Cloudy":
+//                    outfitRecommendation = "It's mostly cloudy so it may rain"
+//                default:
+//                    outfitRecommendation = "No instructions"
+//                }
+//            }
+//        }
+//        
+//        return outfitRecommendation!
+//    }
+    
+//    func selectGear {
+//        if let forecast = self.forecast {
+//            if let currently = forecast.currently {
+//                
+//            }
+//        }
+//    }
+    
+    func selectHeadGear() {
+        if let forecast = self.forecast {
+            if let currently = forecast.currently {
+                print(Int(currently.temperature!))
+                switch Int(currently.temperature!) {
+                case 0..<20: recommendedOutfit.append(Gear.Head.FaceMask.description)
+                case 20..<30: recommendedOutfit.append(Gear.Head.HeavyHat.description)
+                case 80..<200: recommendedOutfit.append(Gear.Head.CapSunglasses.description)
+                case 30..<50: recommendedOutfit.append(Gear.Head.LightHat.description)
+                default: recommendedOutfit.append("No head gear")
                 }
+                
             }
         }
-        
-        return summaryRecommendation!
+    }
+    
+    func recommendationText() -> String {
+        return recommendedOutfit.joinWithSeparator(", ")
     }
 }
