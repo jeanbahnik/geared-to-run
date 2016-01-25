8a8729450348a062b3f66cc11c099c4f85f4d61f
Jean Bahnik <jeanbahnik@gmail.com>
2016-01-25 16:46:33 -0600

    Handling multiple gear constraints

--- WhatToWearRunning/Gear.swift
+++ WhatToWearRunning/Gear.swift
@@ -8,25 +8,38 @@
 
 import Foundation
 
-struct GearItem {
-    let description: String
+struct GearConstraints {
+    let gearType: GearItem
     let minTemp: Int
     let maxTemp: Int
     let minWind: Int?
     let rain: Float?
 }
 
-class Gear {
-    var gearList = [GearItem]()
+struct GearItem {
+    let description: String
+}
+
+class GearList {
+    var gearList = [GearConstraints]()
 
     init() {
-        let capSunglasses = GearItem(description: "Cap or Sunglasses", minTemp: 80, maxTemp: 200, minWind: nil, rain: 0.0)
-        gearList.append(capSunglasses)
-        let lightHat = GearItem(description: "Light Hat", minTemp: 30, maxTemp: 50, minWind: nil, rain: 0.0)
-        gearList.append(lightHat)
-        let heavyHat = GearItem(description: "Heavy Hat", minTemp: 20, maxTemp: 30, minWind: nil, rain: 0.0)
-        gearList.append(heavyHat)
-        let faceMask = GearItem(description: "Face Mask", minTemp: 0, maxTemp: 10, minWind: 10, rain: 0.0)
-        gearList.append(faceMask)
+        let capSunglasses = GearItem(description: "Cap or Sunglasses")
+        let capSunglasses1 = GearConstraints(gearType: capSunglasses, minTemp: 80, maxTemp: 200, minWind: nil, rain: nil)
+        gearList.append(capSunglasses1)
+        let lightHat = GearItem(description: "Light Hat")
+        let lightHat1 = GearConstraints(gearType: lightHat, minTemp: 30, maxTemp: 50, minWind: nil, rain: nil)
+        gearList.append(lightHat1)
+        let heavyHat = GearItem(description: "Heavy Hat")
+        let heavyHat1 = GearConstraints(gearType: heavyHat, minTemp: 20, maxTemp: 30, minWind: nil, rain: nil)
+        gearList.append(heavyHat1)
+        let faceMask = GearItem(description: "Face Mask")
+        let faceMask1 = GearConstraints(gearType: faceMask, minTemp: 0, maxTemp: 10, minWind: 10, rain: nil)
+        gearList.append(faceMask1)
+        let tshirt = GearItem(description: "T-shirt")
+        let tshirt1 = GearConstraints(gearType: tshirt, minTemp: 20, maxTemp: 200, minWind: nil, rain: nil)
+        gearList.append(tshirt1)
+        let tshirt2 = GearConstraints(gearType: tshirt, minTemp: 20, maxTemp: 200, minWind: nil, rain: nil)
+        gearList.append(tshirt2)
     }
 }
--- WhatToWearRunning/Recommendations.swift
+++ WhatToWearRunning/Recommendations.swift
@@ -12,12 +12,12 @@ import ForecastIO
 class Recommendations {
 
     let currentForecast: DataPoint?
-    let gearList: [GearItem]?
+    let gearList: [GearConstraints]?
     var recommendedOutfit = [String]()
 
     init(forecast: Forecast) {
         self.currentForecast = Weather(forecast: forecast).currentForecast
-        self.gearList = Gear().gearList
+        self.gearList = GearList().gearList
     }
 
     func getRecommendedOutfit() {
@@ -28,31 +28,42 @@ class Recommendations {
         }
     }
 
-    func getGear(gear: GearItem) {
+    func getGear(gear: GearConstraints) {
+        if recommendedOutfit.contains(gear.gearType.description) { return }
+        
         var include = false
+        
         if let currentForecast = self.currentForecast {
+            
             if let temperature = currentForecast.temperature, let windSpeed = currentForecast.windSpeed, let precipitationProbability = currentForecast.precipProbability {
+                
                 if gear.minTemp ..< gear.maxTemp ~=
                     Int(temperature) {
                     include = true
-                }
-                if let wind = gear.minWind {
+                } else {
                     include = false
+                }
+                
+                if let wind = gear.minWind {
                     if Int(windSpeed) >= wind {
                         include = true
+                    } else {
+                        include = false
                     }
                 }
+                
                 if let rain = gear.rain {
-                    include = false
                     if rain >= precipitationProbability {
                         include = true
+                    } else {
+                        include = false
                     }
                 }
             }
         }
         
         if include == true {
-            recommendedOutfit.append(gear.description)
+            recommendedOutfit.append(gear.gearType.description)
         }
     }
 
