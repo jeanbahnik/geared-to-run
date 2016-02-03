e270a0bba919d3e2729527dcd855a4aeeea7241b
Jean Bahnik <jeanbahnik@gmail.com>
2016-02-03 10:10:18 -0600

    New Style class for colors, etc...

---
+++ WhatToWearRunning/LaunchViewController.swift
@@ -0,0 +1,9 @@
+//
+//  LaunchViewController.swift
+//  WhatToWearRunning
+//
+//  Created by Jean Bahnik on 2/1/16.
+//  Copyright © 2016 Jean Bahnik. All rights reserved.
+//
+
+import Foundation
---
+++ WhatToWearRunning/Style.swift
@@ -0,0 +1,13 @@
+//
+//  Style.swift
+//  WhatToWearRunning
+//
+//  Created by Jean Bahnik on 2/2/16.
+//  Copyright © 2016 Jean Bahnik. All rights reserved.
+//
+
+class Style {
+    
+    static let navyBlueColor = UIColor(red: 0.0, green: 31.0/255.0, blue: 63.0/255.0, alpha: 1)
+
+}
