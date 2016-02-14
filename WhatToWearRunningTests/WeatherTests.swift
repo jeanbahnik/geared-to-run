f9c17592ea586cabf3d4a2f226ffa2ca849e7fd8
Jean Bahnik <jeanbahnik@gmail.com>
2016-02-14 09:40:13 -0600

    Tests!

--- WhatToWearRunningTests/WeatherTests.swift
+++ WhatToWearRunningTests/WeatherTests.swift
@@ -8,28 +8,28 @@
 
 import XCTest
 
+@testable import WhatToWearRunning
+
 class WeatherTests: XCTestCase {
+    var weather:Weather?
 
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+
+        self.weather = Weather()
+}
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testWindBearingNorth() {
+        XCTAssertEqual(weather!.windBearing(0.0), "Wind from N")
+        XCTAssertEqual(weather!.windBearing(315.0), "Wind from N")
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testWindBearingEast() {
+        XCTAssertEqual(weather!.windBearing(45.0), "Wind from E")
     }
-
 }
