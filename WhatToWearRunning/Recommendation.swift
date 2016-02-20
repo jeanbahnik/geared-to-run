f265fc9e8ec48cdd9006e86d114edf768c9d642d
Jean Bahnik <jeanbahnik@gmail.com>
2016-02-20 11:06:27 -0600

    Code and file structure cleanup

--- WhatToWearRunning/Recommendation.swift
+++ WhatToWearRunning/Recommendation.swift
@@ -9,8 +9,6 @@
 import ForecastIO
 
 class Recommendation {
-
-//    var weather: Weather?
     let gearList: [GearConstraints]?
     var recommendedOutfit: [String] = []
     var recommendationText: String?
@@ -20,39 +18,30 @@ class Recommendation {
     }
 
     func getRecommendedOutfit(weather: Weather?, completion: (recommendation: Recommendation) -> Void) {
-        if let weather = weather {
-            if let currentWeather = weather.currentForecast {
-//        self.currentForecast = currentForecast
-            
-                if let list = gearList {
-                    for item in list {
-                        getGear(item, currentForecast: currentWeather)
-                    }
-                    self.recommendationText = getRecommendationText()
-                    completion(recommendation: self)
+        if let weather = weather, currentWeather = weather.currentForecast {
+            if let list = gearList {
+                for item in list {
+                    getGear(item, currentForecast: currentWeather)
                 }
+                self.recommendationText = getRecommendationText()
+                completion(recommendation: self)
             }
         }
     }
 
     func getGear(gear: GearConstraints, currentForecast: DataPoint) {
-//        self.currentForecast = Weather(forecast: DataPoint).currentForecast
         if recommendedOutfit.contains(gear.gearType.description) { return }
         
         var include = false
-        
-//        if let currentForecast = self.currentForecast {
-        
-            if let temperature = currentForecast.temperature, windSpeed = currentForecast.windSpeed, precipitationProbability = currentForecast.precipProbability {
+        if let temperature = currentForecast.temperature, windSpeed = currentForecast.windSpeed, precipitationProbability = currentForecast.precipProbability {
 
-                if (gear.minTemp ..< gear.maxTemp ~= Int(temperature)) && (Int(windSpeed) >= gear.minWind) && (gear.rain >= precipitationProbability) {
-                        include = true
-                    } else {
-                        include = false
-                    }
-//            }
+            if (gear.minTemp ..< gear.maxTemp ~= Int(temperature)) && (Int(windSpeed) >= gear.minWind) && (gear.rain >= precipitationProbability) {
+                    include = true
+                } else {
+                    include = false
+                }
         }
-        
+
         if include == true {
             recommendedOutfit.append(gear.gearType.description)
         }
