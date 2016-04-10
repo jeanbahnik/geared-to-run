ce45309e5a73d0b2cd3c0af8d204432c54150444
jeanbahnik <jeanbahnik@gmail.com>
2016-04-10 13:12:29 -0500

    wip

--- WhatToWearRunning/GoProViewController.swift
+++ WhatToWearRunning/GoProViewController.swift
@@ -6,9 +6,40 @@
 //  Copyright © 2016 Jean Bahnik. All rights reserved.
 //
 
+import SwiftyStoreKit
+
 class GoProViewController: UIViewController {
+    
+    let kProductID = "com.jeanbahnik.whattowearrunning.gopro"
 
     @IBAction func dismissButtonTapped(sender: UIBarButtonItem) {
         dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    @IBAction func goProButtonTapped(sender: UIButton) {
+        purchaseProduct()
+    }
+    
+    func retrieveProductInto() {
+        SwiftyStoreKit.retrieveProductInfo(kProductID) { result in
+            switch result {
+            case .Success(let product):
+                let priceString = NSNumberFormatter.localizedStringFromNumber(product.price, numberStyle: .CurrencyStyle)
+                print("Product: \(product.localizedDescription), price: \(priceString)")
+            case .Error(let error):
+                print("Error: \(error)")
+            }
+        }
+    }
+
+    func purchaseProduct() {
+        SwiftyStoreKit.purchaseProduct(kProductID) { result in
+            switch result {
+            case .Success(let productId):
+                print("Purchase Success: \(productId)")
+            case .Error(let error):
+                print("Purchase Failed: \(error)")
+            }
+        }
     }
 }
